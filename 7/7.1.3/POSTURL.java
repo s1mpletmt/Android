0a978ec142a28618cf0f9a0deb72b96faee7f62e
Yan Fang <yan.fang@amlogic.com>
2020-07-16 16:37:11 +0800

    change 7.1.3

--- 7/7.1.3/POSTURL.java
+++
@@ -1,8 +0,0 @@
-HttpPut httpPut = new HttpPut(PUTACKCODE_URL);
-httpPut.setHeader(“Content-Type”, "application/json; charset=UTF-8");
-List<NameValuePair> params = new ArrayList<NameValuePair>();
-params.add(new BasicNameValuePair("activation_code", actCode));
-params.add(new BasicNameValuePair("session", new JSONObject().put("id", cookie).toString()));
-entity.setContentType("application/json");
-httpPut.setEntity(entity);
-HttpResponse course_response = new DefaultHttpClient().execute(httpPut);
